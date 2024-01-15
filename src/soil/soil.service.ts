09a6b5f2e8cb135b09f19378d0f35e966cd84c8e
Dimitar Mitev <dimitar.mitev@yara.com>
2024-01-15 11:21:42 +0200

    Soil validation

--- src/soil/soil.service.ts
+++ src/soil/soil.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Soil } from './soil.entity';
@@ -11,25 +15,48 @@ export class SoilService {
     private readonly soilRepository: Repository<Soil>,
   ) {}
 
+  private async checkSoilExists(id: string): Promise<Soil> {
+    const soil = await this.soilRepository.findOne({
+      where: { id },
+    });
+    if (!soil) {
+      throw new NotFoundException("Crop with this id doesn't exist");
+    }
+    return soil;
+  }
+
   async findAll(): Promise<Soil[]> {
     return this.soilRepository.find();
   }
 
   async findOne(id: string): Promise<Soil> {
-    return this.soilRepository.findOne({ where: { id } });
+    return this.checkSoilExists(id);
   }
 
   async create(createSoilDto: CreateSoilDto): Promise<Soil> {
-    const newSoil = this.soilRepository.create(createSoilDto);
-    return await this.soilRepository.save(newSoil);
+    const soil = new Soil();
+    const soilName = await this.soilRepository.findOne({
+      where: { soil: createSoilDto.soil },
+    });
+    if (soilName) {
+      throw new BadRequestException(
+        'Soil with this name is already exist. Change it!',
+      );
+    }
+    soil.soil = createSoilDto.soil;
+    return await this.soilRepository.save(soil);
   }
 
   async update(id: string, updateSoilDto: CreateSoilDto): Promise<Soil> {
-    await this.soilRepository.update(id, updateSoilDto);
-    return await this.soilRepository.findOne({ where: { id } });
+    const soil = await this.checkSoilExists(id);
+    soil.soil = updateSoilDto.soil;
+    return await this.soilRepository.save(soil);
   }
 
   async remove(id: string): Promise<void> {
-    await this.soilRepository.update(id, { deletedAt: new Date() });
+    const soil = await this.checkSoilExists(id);
+    await this.soilRepository.update(soil.id, {
+      deletedAt: new Date(),
+    });
   }
 }

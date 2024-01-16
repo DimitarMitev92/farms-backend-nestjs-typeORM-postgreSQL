91a586155d85767da4eb4b376e134e21df621442
Dimitar Mitev <dimitar.mitev@yara.com>
2024-01-16 19:25:10 +0200

    User add try,catch

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DeepPartial, Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -13,44 +17,76 @@ export class UserService {
   ) {}
 
   async findAll(): Promise<User[]> {
-    return this.userRepository.find({ where: { deletedAt: null } });
+    try {
+      return this.userRepository.find({ where: { deletedAt: null } });
+    } catch (error) {
+      throw new InternalServerErrorException('Error while fetching users');
+    }
   }
 
   async findOne(id: string): Promise<User> {
-    return this.userRepository.findOne({ where: { id, deletedAt: null } });
+    try {
+      return this.userRepository.findOne({ where: { id, deletedAt: null } });
+    } catch (error) {
+      throw new InternalServerErrorException('Error while fetching user');
+    }
   }
 
   async findOneByEmail(email: string): Promise<User> {
-    return this.userRepository.findOne({
-      where: { email: email, deletedAt: null },
-    });
+    try {
+      return this.userRepository.findOne({
+        where: { email: email, deletedAt: null },
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Error while fetching user by email',
+      );
+    }
   }
 
-  async create(data: DeepPartial<User>) {
-    const user = this.userRepository.create(data);
-    return await this.userRepository.save(user);
+  async create(data: DeepPartial<User>): Promise<User> {
+    try {
+      const user = this.userRepository.create(data);
+      return await this.userRepository.save(user);
+    } catch (error) {
+      throw new InternalServerErrorException('Error while creating user');
+    }
   }
 
   async update(id: string, data: DeepPartial<User>): Promise<User> {
-    const existingUser = await this.findOne(id);
-    if (!existingUser) {
-      throw new NotFoundException('User not found');
+    try {
+      const existingUser = await this.findOne(id);
+      if (!existingUser) {
+        throw new NotFoundException('User not found');
+      }
+
+      Object.assign(existingUser, data);
+
+      return await this.userRepository.save(existingUser);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Error while updating user');
     }
-
-    Object.assign(existingUser, data);
-
-    return await this.userRepository.save(existingUser);
   }
 
   async remove(id: string): Promise<void> {
-    const user = await this.findOne(id);
+    try {
+      const user = await this.findOne(id);
 
-    if (!user) {
-      throw new NotFoundException('User not found');
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+
+      user.deletedAt = new Date();
+
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Error while removing user');
     }
-
-    user.deletedAt = new Date();
-
-    await this.userRepository.save(user);
   }
 }

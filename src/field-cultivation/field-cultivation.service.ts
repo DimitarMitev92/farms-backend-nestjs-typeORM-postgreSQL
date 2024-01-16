d7fa81af466f4e9cf20e6749262e9f755504876c
Dimitar Mitev <dimitar.mitev@yara.com>
2024-01-16 19:17:55 +0200

    field cultivation add try, catch

--- src/field-cultivation/field-cultivation.service.ts
+++ src/field-cultivation/field-cultivation.service.ts
@@ -7,6 +7,7 @@ import {
   BadRequestException,
   Injectable,
   NotFoundException,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { CreateFieldCultivationDto } from './dto/create-field-cultivation.dto';
 import { GrowingProcess } from 'src/growing-process/growing-process.entity';
@@ -27,99 +28,149 @@ export class FieldCultivationService {
   private async checkFieldCultivationExists(
     id: string,
   ): Promise<FieldCultivation> {
-    const fieldCultivation = await this.fieldCultivationRepository.findOne({
-      where: { id, deletedAt: null },
-    });
-    if (!fieldCultivation) {
-      throw new NotFoundException(
-        "Field cultivation with this id doesn't exist",
+    try {
+      const fieldCultivation = await this.fieldCultivationRepository.findOne({
+        where: { id, deletedAt: null },
+      });
+      if (!fieldCultivation) {
+        throw new NotFoundException(
+          "Field cultivation with this id doesn't exist",
+        );
+      }
+      return fieldCultivation;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Error while fetching field cultivation data',
       );
     }
-    return fieldCultivation;
   }
 
-  findAll(): Promise<FieldCultivation[]> {
-    return this.fieldCultivationRepository.find();
+  async findAll(): Promise<FieldCultivation[]> {
+    try {
+      return this.fieldCultivationRepository.find();
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Error while fetching field cultivations',
+      );
+    }
   }
 
-  findOne(id: string): Promise<FieldCultivation> {
+  async findOne(id: string): Promise<FieldCultivation> {
     return this.checkFieldCultivationExists(id);
   }
 
   async create(
     createFieldCultivationDto: CreateFieldCultivationDto,
   ): Promise<FieldCultivation> {
-    const fieldCultivation = new FieldCultivation();
-    const cultivationIdExist = await this.cultivationRepository.findOne({
-      where: { id: createFieldCultivationDto.cultivationId, deletedAt: null },
-    });
-    if (!cultivationIdExist) {
-      throw new BadRequestException('Invalid cultivation id.');
+    try {
+      const fieldCultivation = new FieldCultivation();
+      const cultivationIdExist = await this.cultivationRepository.findOne({
+        where: { id: createFieldCultivationDto.cultivationId, deletedAt: null },
+      });
+      if (!cultivationIdExist) {
+        throw new BadRequestException('Invalid cultivation id.');
+      }
+      const machineryIdExist = await this.machineryRepository.findOne({
+        where: { id: createFieldCultivationDto.machineryId, deletedAt: null },
+      });
+      if (!machineryIdExist) {
+        throw new BadRequestException('Invalid machinery id.');
+      }
+      const growingProcessIdExist = await this.growingProcessRepository.findOne(
+        {
+          where: {
+            id: createFieldCultivationDto.growingProcessId,
+            deletedAt: null,
+          },
+        },
+      );
+      if (!growingProcessIdExist) {
+        throw new BadRequestException('Invalid growing process id.');
+      }
+      fieldCultivation.startingDate = createFieldCultivationDto.startingDate;
+      fieldCultivation.cultivationId = createFieldCultivationDto.cultivationId;
+      fieldCultivation.machineryId = createFieldCultivationDto.machineryId;
+      fieldCultivation.growingProcessId =
+        createFieldCultivationDto.growingProcessId;
+      return await this.fieldCultivationRepository.save(fieldCultivation);
+    } catch (error) {
+      if (
+        error instanceof BadRequestException ||
+        error instanceof NotFoundException
+      ) {
+        throw error;
+      }
+      throw new InternalServerErrorException(
+        'Error while creating field cultivation',
+      );
     }
-    const machineryIdExist = await this.machineryRepository.findOne({
-      where: { id: createFieldCultivationDto.machineryId, deletedAt: null },
-    });
-    if (!machineryIdExist) {
-      throw new BadRequestException('Invalid machinery id.');
-    }
-    const growingProcessIdExist = await this.growingProcessRepository.findOne({
-      where: {
-        id: createFieldCultivationDto.growingProcessId,
-        deletedAt: null,
-      },
-    });
-    if (!growingProcessIdExist) {
-      throw new BadRequestException('Invalid growing process id.');
-    }
-    fieldCultivation.startingDate = createFieldCultivationDto.startingDate;
-    fieldCultivation.cultivationId = createFieldCultivationDto.cultivationId;
-    fieldCultivation.machineryId = createFieldCultivationDto.machineryId;
-    fieldCultivation.growingProcessId =
-      createFieldCultivationDto.growingProcessId;
-    return await this.fieldCultivationRepository.save(fieldCultivation);
   }
 
   async update(
     id: string,
     updateFieldCultivationDto: Partial<CreateFieldCultivationDto>,
   ): Promise<FieldCultivation> {
-    const fieldCultivation = await this.fieldCultivationRepository.findOne({
-      where: { id, deletedAt: null },
-    });
-    if (!fieldCultivation) {
-      throw new Error(`Field cultivation with id ${id} not found`);
+    try {
+      const fieldCultivation = await this.fieldCultivationRepository.findOne({
+        where: { id, deletedAt: null },
+      });
+      if (!fieldCultivation) {
+        throw new NotFoundException(
+          `Field cultivation with id ${id} not found`,
+        );
+      }
+
+      const cultivationIdExist = await this.cultivationRepository.findOne({
+        where: { id: updateFieldCultivationDto.cultivationId, deletedAt: null },
+      });
+      if (!cultivationIdExist) {
+        throw new BadRequestException('Invalid cultivation id.');
+      }
+
+      const machineryIdExist = await this.machineryRepository.findOne({
+        where: { id: updateFieldCultivationDto.machineryId, deletedAt: null },
+      });
+      if (!machineryIdExist) {
+        throw new BadRequestException('Invalid machinery id.');
+      }
+
+      const growingProcessIdExist = await this.growingProcessRepository.findOne(
+        {
+          where: {
+            id: updateFieldCultivationDto.growingProcessId,
+            deletedAt: null,
+          },
+        },
+      );
+      if (!growingProcessIdExist) {
+        throw new BadRequestException('Invalid growing process id.');
+      }
+
+      Object.assign(fieldCultivation, updateFieldCultivationDto);
+
+      return await this.fieldCultivationRepository.save(fieldCultivation);
+    } catch (error) {
+      if (
+        error instanceof NotFoundException ||
+        error instanceof BadRequestException
+      ) {
+        throw error;
+      }
+      throw new InternalServerErrorException(
+        'Error while updating field cultivation',
+      );
     }
-
-    const cultivationIdExist = await this.cultivationRepository.findOne({
-      where: { id: updateFieldCultivationDto.cultivationId, deletedAt: null },
-    });
-    if (!cultivationIdExist) {
-      throw new BadRequestException('Invalid cultivation id.');
-    }
-
-    const machineryIdExist = await this.machineryRepository.findOne({
-      where: { id: updateFieldCultivationDto.machineryId, deletedAt: null },
-    });
-    if (!machineryIdExist) {
-      throw new BadRequestException('Invalid machinery id.');
-    }
-
-    const growingProcessIdExist = await this.growingProcessRepository.findOne({
-      where: {
-        id: updateFieldCultivationDto.growingProcessId,
-        deletedAt: null,
-      },
-    });
-    if (!growingProcessIdExist) {
-      throw new BadRequestException('Invalid growing process id.');
-    }
-
-    Object.assign(fieldCultivation, updateFieldCultivationDto);
-
-    return await this.fieldCultivationRepository.save(fieldCultivation);
   }
 
   async remove(id: string): Promise<void> {
-    await this.fieldCultivationRepository.update(id, { deletedAt: new Date() });
+    try {
+      await this.fieldCultivationRepository.update(id, {
+        deletedAt: new Date(),
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Error while removing field cultivation',
+      );
+    }
   }
 }
